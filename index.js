bf2aff1cc82568040becab19991a0940e7bc59bd
Michael Shyu <michaelshyu11@gmail.com>
2024-01-13 17:19:39 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function (work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(adjective = '*') {
+    return function (param1 = 'special') {
+        return `You are ${adjective}${param1}${adjective}!`
+    }
+}
